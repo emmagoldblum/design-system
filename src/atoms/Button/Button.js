3769dba0bad4f99bfbca91ca491697928266b33a
Emma Goldblum <emmgoldblum@gmail.com>
2020-01-16 21:47:23 +0100

    adding simple button atoms to the design system

--- src/atoms/Button/Button.js
+++ src/atoms/Button/Button.js
@@ -1,1 +1,22 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import './Button.scss'
 
+export const Button = ({ type, buttonVariant, disabled, onClick, title }) => <button className={`btn-${buttonVariant} ${type}`} disabled={disabled} onClick={onClick}>{title}</button>;
+
+
+Button.propTypes = {
+    type: PropTypes.string,
+    buttonVariant: PropTypes.string,
+    disabled: PropTypes.bool,
+    onClick: PropTypes.func.isRequired,
+    title: PropTypes.string.isRequired
+};
+
+Button.defaultProps = {
+    type: 'default',
+    buttonVariant: 'flat',
+    disabled: false
+};
+
+export default Button
